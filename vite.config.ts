cc3dd4a4881936321f66f2a57f0e0547b9920be4
Yannick Bley <bleyyannickpro@gmail.com>
2024-06-11 11:18:11 +0200

    fixing wrong base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bleyyannick/FM_e-commerce-product-page/'
+  base: '/FM_e-commerce-product-page/'
 })
