252423334d8b088e754b94798fe7523461b98a27
Yannick Bley <bleyyannickpro@gmail.com>
2024-06-14 18:06:17 +0200

    add how to add and substract product number

--- src/components/ManageProduct/ManageProduct.tsx
+++ src/components/ManageProduct/ManageProduct.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import './ManageProduct.css'
 
 export const ManageProduct = () => {
+    const [productNumber, setProductNumber] = useState<number>(0)
+
+    const substractProductNumber = () => {
+        if (productNumber > 0) {
+            setProductNumber(productNumber - 1)
+        } else {
+            setProductNumber(0)
+        }
+    }
+    const addProductNumber = () => setProductNumber(productNumber + 1)
+    
     return (
         <div className='manageProduct'>
-            <button> - </button>
-            <span> 0 </span>
-            <button> + </button>
+            <button onClick={substractProductNumber}> - </button>
+            <span>{productNumber}</span>
+            <button onClick={addProductNumber}> + </button>
         </div>
     )
 }
